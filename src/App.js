c8efa8da9af1f334420ebfa6c21641f40a518b98
Naveed Nadjmabadi <naveed.nadjmabadi@gmail.com>
2019-03-02 21:38:59 -0800

    recreate everything with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Terminal from 'terminal-in-react';
+import Emoji from 'a11y-react-emoji'
+
+
+const Banner = () => (
+  <div id="banner">
+    <pre className="banner">{`
+    ███╗   ██╗ █████╗ ██╗   ██╗███████╗███████╗██████╗    ███████╗██╗  ██╗
+    ████╗  ██║██╔══██╗██║   ██║██╔════╝██╔════╝██╔══██╗   ██╔════╝██║  ██║
+    ██╔██╗ ██║███████║██║   ██║█████╗  █████╗  ██║  ██║   ███████╗███████║
+    ██║╚██╗██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══╝  ██║  ██║   ╚════██║██╔══██║
+    ██║ ╚████║██║  ██║ ╚████╔╝ ███████╗███████╗██████╔╝██╗███████║██║  ██║
+    ╚═╝  ╚═══╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚══════╝╚═════╝ ╚═╝╚══════╝╚═╝  ╚═╝
+  `}</pre>
+</div>)
+
+
+const Footer = () => (
+  <footer>
+    <p>  Made with
+            {' '}
+            <Emoji symbol="❤️" label="love" />
+            {' '}
+            by Naveed Nadjmabadi</p>
+    <p>Github: <a href="https://github.com/naveedn">naveedn</a> | Twitter: <a
+        href="https://twitter.com/nudgemybody">@nudgemybody</a></p>
+  </footer>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Banner />
         </header>
+        <Terminal
+          color='#41FF00'
+          backgroundColor='black'
+          barColor='black'
+          hideTopBar={true}
+          allowTabs={false}
+          style={{ fontFamily: 'source-code-pro, Menlo, Monaco, Consolas, "Courier New"', fontWeight: "bold", fontSize: "1em", textAlign: "left", marginLeft: "2em" }}
+          commands={{
+            'open-google': () => window.open('https://www.google.com/', '_blank'),
+            showmsg: this.showMsg,
+            popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'open-google': 'opens google.com',
+            showmsg: 'shows a message',
+            alert: 'alert', popup: 'alert'
+          }}
+          msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+        />
+        <Footer />
       </div>
     );
   }
